415930d66d0a0659e73f60296bd48f69bf03e970
Túlio Castro Silva <tulio.castro14@gmail.com>
2023-07-27 23:30:56 -0300

    .

--- main.py
+++ main.py
@@ -1,174 +1,217 @@
-from pygame.locals import *
-from operator import add, sub
 import pygame
-import sys
-import math
+from pygame.locals import *
+from math import sin, cos, sqrt, radians
 import random
 
+
+
+#-----------------------COMANDOS-----------------------
+
+#Clique na tecla R -> Gera novas paredes aleatoriamente
+
+#Clique na tecla C -> Limpa todas as paredes
+
+#Clique na tecla N -> Cria uma nova parede    
+#Ao clicar n, os raios de luz deixarao de ser projetados
+#Use o botao direito do mouse para selecionar as coordenadas 
+#da parede
+
+#------------------------------------------------------
+
+
+#Iniciando a janela 
 pygame.init()
-
-WINDOW_SIZE = (800, 600)     # Altura x Largura em pixels
-NUM_RAYS = 360               # Entre 1 e 360 
-SOLID_RAYS = False           # Estranho, melhores resultados com NUM_RAYS = 360
-NUM_WALLS = 10                # The amount of randomly generated walls
-DISPLAY_MODE = "showcase"    # "darkness" or "showcase" or ""
+height = 600
+width = 600
+win = pygame.display.set_mode((height,width))
 
 
 
-screen = pygame.display.set_mode(WINDOW_SIZE)
-display = pygame.Surface(WINDOW_SIZE)
-
-mx, my = pygame.mouse.get_pos()
-lastClosestPoint = (0, 0)
-running = True
-rays = []
-walls = []
-particles = []
+#Variaveis
+color = (0,0,0)
+num_rays = 360
+num_walls = 5
 
 
-if DISPLAY_MODE == "darkness":
-    display_color = (255, 255, 255)
-    ray_color = "black"
-    wall_color = "white"
-
-elif DISPLAY_MODE == "showcase":
-    display_color = (0, 0, 0)
-    ray_color = "white"
-    wall_color = "white"  
-
-elif DISPLAY_MODE == "":
-    display_color = (255, 255, 255)  #in RGB
-    ray_color = "black"
-    wall_color = "white"
-
-class Ray:
-    def __init__(self, x, y, angle):
+class Particle:
+    def __init__(self, x, y):
+        """Construtor da classe particula
+        float, float -> Particle"""
         self.x = x
         self.y = y
-        self.dir = (math.cos(angle), math.sin(angle))
+        self.rays = []
+    
+    def update(self,x,y):
+        """Funcao que atualiza a posicao da particula e seus raios
+        float, float -> None"""
+        self.x = x
+        self.y = y
+        for ray in self.rays:
+            ray.move(self.x,self.y)
+        
+    
+    def look_for_walls(self,walls):
+        """Funcao que checa se um raio colidiu com uma parede, desenha sua trajetoria
+        list -> None"""
+        for i in range(len(self.rays)):
+            closest = None                                  #Ponto mais perto
+            record = 100000000000                           #Distancia da particula ate o ponto mais perto
+            for wall in walls:
+                point = self.rays[i].collision(wall)       
+                if point:
+                    distance = sqrt((self.rays[i].x - point[0])**2 + (self.rays[i].y - point[1])**2)
+                    if distance < record:
+                        record = distance
+                        closest = point
+            if closest:
+                pygame.draw.line(win,color,(self.rays[i].x,self.rays[i].y),closest)
+                    
 
-    def update(self, mx, my):
+class Ray:
+    def __init__(self,x,y,angle):
+        """Construtor da classe Ray
+        float, float, float -> Ray"""
+        self.x = x
+        self.y = y
+        self.dir = (sin(angle),cos(angle))
+    
+    def move(self,mx,my):
+        """Funcao que move o raio de luz
+        float, float -> None"""
         self.x = mx
         self.y = my
+    
+    def collision(self,wall):
+        """Funcao que checa se o raio de luz colidiu com uma parede
+        Wall -> Tuple/Bool """
 
-    def checkCollision(self, wall):
-        x1 = wall.start_pos[0]
-        y1 = wall.start_pos[1]
-        x2 = wall.end_pos[0]
-        y2 = wall.end_pos[1]
+        #Coordenadas da parede
+        x1 = wall.x1
+        y1 = wall.y1
+        x2 = wall.x2
+        y2 = wall.y2
 
+
+        #Coordenadas do raio de luz
         x3 = self.x
         y3 = self.y
         x4 = self.x + self.dir[0]
         y4 = self.y + self.dir[1]
-    
-        # Using line-line intersection formula to get intersection point of ray and wall
-        # Where (x1, y1), (x2, y2) are the ray pos and (x3, y3), (x4, y4) are the wall pos
-        denominador = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-        numerador = (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)
-        if denominador == 0:
-            return None
-        
-        t = numerador / denominador
-        u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denominador
 
-        if 1 > t > 0 and u > 0:
+
+        #Utilizando a formula de "line-line intersection" para 
+        #Determinar se o raio de luz colide com a parede
+        den = ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
+
+        if den == 0:
+            return False
+
+        t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den
+        u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den
+
+        if (t > 0 and t < 1 and u > 0):
             x = x1 + t * (x2 - x1)
             y = y1 + t * (y2 - y1)
-            collidePos = [x, y]
-            return collidePos
+            return (x,y)
+        else:
+            return False
+
 
 class Wall:
-    def __init__(self, start_pos, end_pos, color = wall_color):
-        self.start_pos = start_pos
-        self.end_pos = end_pos
-        self.color = color
-        self.slope_x = end_pos[0] - start_pos[0]
-        self.slope_y = end_pos[1] - start_pos[1]
-        if self.slope_x == 0:
-            self.slope = 0
-        else:
-            self.slope = self.slope_y / self.slope_x
-        self.length = math.sqrt(self.slope_x**2 + self.slope_y**2)
-
+    def __init__(self,x1,y1,x2,y2):
+        """Construtor da classe Wall recebe as coordenadas da parede
+        float, float, float, float -> Wall"""
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    
     def draw(self):
-        pygame.draw.line(display, self.color, self.start_pos, self.end_pos, 3)
+        """Desenha a parede
+        None -> None"""
+        pygame.draw.line(win,color,(self.x1, self.y1),(self.x2 ,self.y2))
 
-for i in range(0, 360, int(360/NUM_RAYS)):
-    rays.append(Ray(mx, my, math.radians(i)))
-
-def drawRays(rays, walls, color = ray_color):
-    global lastClosestPoint
-    for ray in rays:
-        closest = 100000
-        closestPoint = None
-        for wall in walls:
-            intersectPoint = ray.checkCollision(wall)
-            if intersectPoint is not None:
-                # Get distance between ray source and intersect point
-                ray_dx = ray.x - intersectPoint[0]
-                ray_dy = ray.y - intersectPoint[1]
-                # If the intersect point is closer than the previous closest intersect point, it becomes the closest intersect point
-                distance = math.sqrt(ray_dx**2 + ray_dy**2)
-                if (distance < closest):
-                    closest = distance
-                    closestPoint = intersectPoint
-
-        if closestPoint is not None:
-            pygame.draw.line(display, color, (ray.x, ray.y), closestPoint)
-            if SOLID_RAYS:
-                pygame.draw.polygon(display, color, [(mx, my), closestPoint, lastClosestPoint])
-                lastClosestPoint = closestPoint
 
 def generateWalls():
+    """Funcao que gera aleatoriamente paredes dentro do espaço permitido
+    None -> None"""
     walls.clear()
 
-    walls.append(Wall((0, 0), (WINDOW_SIZE[0], 0)))
-    walls.append(Wall((0, 0), (0, WINDOW_SIZE[1])))
-    walls.append(Wall((WINDOW_SIZE[0], 0), (WINDOW_SIZE[0], WINDOW_SIZE[1])))
-    walls.append(Wall((0, WINDOW_SIZE[1]), (WINDOW_SIZE[0], WINDOW_SIZE[1])))
+    walls.append(Wall(0, 0, height, 0))
+    walls.append(Wall(0, 0, 0, width))
+    walls.append(Wall(height, 0, width, height))
+    walls.append(Wall(0, width, height, width))
 
-    for i in range(NUM_WALLS):
-        start_x = random.randint(0, WINDOW_SIZE[0])
-        start_y = random.randint(0, WINDOW_SIZE[1])
-        end_x = random.randint(0, WINDOW_SIZE[0])
-        end_y = random.randint(0, WINDOW_SIZE[1])
-        walls.append(Wall((start_x, start_y), (end_x, end_y)))
+    for i in range(num_walls):
+        x1 = random.randint(0, height)
+        y1 = random.randint(0, width)
+        x2 = random.randint(0, height)
+        y2 = random.randint(0, width)
+        walls.append(Wall(x1, y1, x2, y2))
 
-def draw():
-    display.fill(display_color)
 
+def Draw(Particle,Walls):
+    """Funcao que desenha as paredes e os raios de luz"""
     for wall in walls:
         wall.draw()
-
-    for particle in particles:
-        particle.draw()
-
-    drawRays([ray for ray in rays], [wall for wall in walls])
-
-    screen.blit(display, (0, 0))
-
-    pygame.display.update()
+    Particle.look_for_walls(walls)
 
 
-if len(walls) == 0:
-    generateWalls()
+
+#Iniciando a particula
+x, y = pygame.mouse.get_pos()
+part = Particle(x,y)
+for i in range(0, 360, int(360/num_rays)):
+    part.rays.append(Ray(x, y,radians(i)))
+
+#Iniciando as paredes
+walls = []
+generateWalls()
+
+running = True                          #Bool para dizer se o progama esta rodando
+casting = True                          #Bool para dizer se estamos projetando os raios de luz ou nao
+new_wall_a = False                      #Variavel utilizada para criar uma nova parede
+new_wall_b = False                      #Variavel utilizada para criar uma nova parede
+
 
 while running:
-    mx, my = pygame.mouse.get_pos()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            sys.exit()
+
+    events = pygame.event.get()                                #Interacoes homem maquina
+    mx, my = pygame.mouse.get_pos()                            #Posicao do mouse
+    left, middle, right = pygame.mouse.get_pressed()           #Bool utilizado para o clique do mouse
+
+    for event in events:
+        if event.type == pygame.QUIT:
             pygame.quit()
-
+            run = False
+            quit()
         if event.type == KEYDOWN:
-            # Re-randomize walls on Space
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_r:                        #Evento de apertar r (gerar novas parede)
                generateWalls()
+            if event.key == pygame.K_c:                        #Evento de apertar c (limpar as paredes)
+                del walls[4:]
+            if event.key == pygame.K_n:                        #Evento de apertar n (nova parede)
+                casting = not casting
+        if event.type == MOUSEBUTTONDOWN:      
+            if left and not casting:
+                if new_wall_a:
+                    new_wall_b = (mx,my)
+                    walls.append(Wall(new_wall_a[0], new_wall_a[1], new_wall_b[0], new_wall_b[1]))
+                    new_wall_a = False
+                    new_wall_b = False
+                    casting = not casting
+                else:
+                    new_wall_a = (mx,my)
+    part.update(mx,my)
+    win.fill((255, 255, 255))
 
-    for ray in rays:
-        ray.update(mx, my)
+    #Desenhando
+    if casting == True:
+        Draw(part,walls)
+    else:
+        if new_wall_a:
+            pygame.draw.line(win,(255,0,0),(new_wall_a[0],new_wall_a[1]),(mx,my))
+        for wall in walls:
+            wall.draw()
 
-    draw()
-
-
+    pygame.display.update()
